38c831fca21f63ff30b4d1339155773a74788de9
elmehdi bouhlal <bouhlalmahdi8@gmail.com>
2026-06-27 22:48:00 +0100

    fix prisma version issue

--- config/prisma.js
+++ config/prisma.js
@@ -1,8 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
 const globalForPrisma = globalThis;
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
