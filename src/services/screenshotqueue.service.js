d41cb6fb4ce3f0495e3f5bdec2598d2e5468636c
elmehdi bouhlal <bouhlalmahdi8@gmail.com>
2026-06-28 18:30:53 +0100

    update the variables name

--- src/services/screenshotqueue.service.js
+++ src/services/screenshotqueue.service.js
@@ -4,29 +4,32 @@ import PageService from './page.service.js';
 import ScreenshotService from './screenshot.service.js';
 
 class ScreenshotQueueService {
-    constructor() {
-        this.flare   = new FlareSolverrService();
-        this.browser = new BrowserService();
-    }
+  constructor() {
+    this.flare = new FlareSolverrService();
+    this.browser = new BrowserService();
+  }
 
-    async capture(url, outDir) {
-        const { cookies, userAgent } = await this.flare.getCookies(url);
-        const rawPage = await this.browser.newPage();
-        const pageService = new PageService(rawPage);
+  async capture(url, captureRequestId) {
+    const { cookies, userAgent } = await this.flare.getCookies(url);
+    const rawPage = await this.browser.newPage();
+    const pageService = new PageService(rawPage);
 
-        try {
-            await pageService.configure(cookies, userAgent);
-            await pageService.blockConsentScripts();
-            await pageService.navigate(url);
-            await pageService.dismissPopups();
-            await pageService.slowScroll();
-            const screenshotService = new ScreenshotService(rawPage);
-            const sectionFiles      = await screenshotService.captureAll(url, outDir);
-            return sectionFiles;
-        } finally {
-            await pageService.close();
-        }
+    try {
+      await pageService.configure(cookies, userAgent);
+      await pageService.blockConsentScripts();
+      await pageService.navigate(url);
+      await pageService.dismissPopups();
+      await pageService.slowScroll();
+
+      const screenshotService = new ScreenshotService(rawPage);
+      const sectionFiles = await screenshotService.captureAll(url, captureRequestId);
+
+      return sectionFiles;
+    } finally {
+      await pageService.close();
+      await this.browser.close();
     }
+  }
 }
 
 export default new ScreenshotQueueService();
